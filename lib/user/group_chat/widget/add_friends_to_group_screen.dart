b225bea12ce145a69b6b675ccf4a36c7711e7214
seniorman-dev <coder.jay2001@gmail.com>
2023-08-19 17:53:54 +0100

    modified:   lib/main.dart 	deleted:    lib/test_audio.dart 	modified:   lib/user/chat/controller/chat_service_controller.dart 	modified:   lib/user/chat/widget/audio/audio_player_widget.dart 	modified:   lib/user/chat/widget/bottom_engine.dart 	modified:   lib/user/chat/widget/chat_list.dart 	modified:   lib/user/chat/widget/recent_chats_list.dart 	modified:   lib/user/chat/widget/send_picture_or_video_dialogue.dart 	modified:   lib/user/chat/widget/video/video_player_widget.dart 	modified:   lib/user/feeds/controller/feeds_controller.dart 	modified:   lib/user/feeds/screen/feeds_screen.dart 	modified:   lib/user/group_chat/screen/group_chat_screen.dart 	modified:   lib/user/settings/screen/profile_screen.dart 	modified:   lib/user/settings/widget/action_screens/edit_profile_screen.dart 	modified:   lib/user/settings/widget/action_screens/upload_post_page.dart 	modified:   lib/user/settings/widget/activities/re-posts.dart 	modified:   lib/user/settings/widget/helper_widgets/view_posts.dart
     Untracked files:
    	lib/user/group_chat/controller/
    	lib/user/group_chat/screen/group_messages_screen.dart
    	lib/user/group_chat/widget/add_members_to_group.dart
    	lib/user/group_chat/widget/bottom_engine_for_group.dart
    	lib/user/group_chat/widget/chat_list_for_group.dart
    	lib/user/group_chat/widget/create_group.dart
    	lib/user/group_chat/widget/search_textfield.dart
    	lib/user/group_chat/widget/send_options_bottom_sheet.dart
    	lib/user/group_chat/widget/successful_group_creation_screen.dart

---
+++ lib/user/group_chat/widget/add_friends_to_group_screen.dart
@@ -0,0 +1,8 @@
+import 'package:pichat/theme/app_theme.dart';
+import 'package:pichat/user/chat/controller/chat_service_controller.dart';
+import 'package:pichat/user/chat/screen/dm_screen.dart';
+import 'package:pichat/utils/error_loader.dart';
+import 'package:pichat/utils/extract_firstname.dart';
+import 'package:pichat/utils/loader.dart';
+import 'package:provider/provider.dart';
+
