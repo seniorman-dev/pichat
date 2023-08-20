c7633fc727321d42bb652dc4f0e8adc173f2ff54
seniorman-dev <coder.jay2001@gmail.com>
2023-08-20 16:02:29 +0100

    modified:   lib/user/chat/screen/dm_screen.dart 	modified:   lib/user/chat/widget/users/all_users_list.dart 	modified:   lib/user/group_chat/controller/group_chat_controller.dart 	modified:   lib/user/group_chat/screen/group_chat_screen.dart 	modified:   lib/user/group_chat/screen/group_messages_screen.dart 	deleted:    lib/user/group_chat/widget/add_friends_to_group_screen.dart 	deleted:    lib/user/group_chat/widget/add_members_to_group.dart 	modified:   lib/user/group_chat/widget/bottom_engine_for_group.dart 	modified:   lib/user/group_chat/widget/chat_list_for_group.dart 	modified:   lib/user/group_chat/widget/create_group.dart 	modified:   lib/user/settings/widget/action_screens/edit_profile_screen.dart 	modified:   lib/user/settings/widget/helper_widgets/insights.dart

---
+++ lib/user/group_chat/widget/add_user_to_group.dart
@@ -0,0 +1,16 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pichat/auth/controller/auth_controller.dart';
+import 'package:pichat/theme/app_theme.dart';
+import 'package:pichat/user/group_chat/controller/group_chat_controller.dart';
+
+
+
+
+
+
